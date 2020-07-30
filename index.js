ff3b08f28c018c49007578c8a13aef614305c819
V S S DHEERAJ KOTTE <kottedheeraj@gmail.com>
2020-07-30 22:00:44 +0530

    Authentication has been added

--- index.js
+++ index.js
@@ -1,36 +1,100 @@
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://Dheeru_0650:@mflix-t3llq.mongodb.net/Grocerys", {useNewUrlParser:true});
-
-const GrocerySchema = new mongoose.Schema({
-  name:String,
-  rating:Number,
-  cost:Number
-});
-
-const Grocery = mongoose.model("Grocery",GrocerySchema);
-
-const grocery = new Grocery({
-  name:"sunflower oil",
-  rating:5,
-  cost:20
-});
-
-grocery.save();
+const encrypt = require('mongoose-encryption');
 const app = express();
 // app.use("view engine","ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get("/",function(req,res){
-  res.render("item",{});
+mongoose.connect("mongodb+srv://Dheeru_0650:@mflix-t3llq.mongodb.net/Grocerys", {
+  useNewUrlParser: true
 });
 
-app.listen(2000,function(){
-  console.log("server started");
+const userSchema = {
+  username: String,
+  email: String,
+  password: String
+};
+
+const GrocerySchema = new mongoose.Schema({
+
+  name: String,
+
+  Actual_price: Number,
+
+  discount: Number,
+
+  final_price: Number,
+
+  count: Number,
+
+  quantity: [String],
+
+  Highlights: [
+    String
+  ],
+
+  description: String,
+
+  Specifications: [
+    [String],
+  ],
+
+  images: [String],
+  rating: Number,
+  rating_count: Number
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    username: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  newUser.save(function(error) {
+    if (error) {
+
+    } else {
+
+    }
+  });
+
+});
+
+app.post("/login", function(req, res) {
+
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+
+  User.findOne({email: email}, function(error, foundUser){
+    if (error) {
+      console.log(error);
+    } else {
+      if (foundUser.password === password) {
+        console.log("you are in");
+      }
+    }
+  });
+
+});
+
+
+app.listen(2000, function() {
+  console.log("connected");
 });
