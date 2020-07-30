e0caf550d5d3202bd1c0cc228f0fb8712a2e92c3
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2020-07-30 22:16:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb+srv://:@mflix-t3llq.mongodb.net/Grocerys", {
+mongoose.connect("mongodb+srv://Grocerys", {
   useNewUrlParser: true
 });
 
