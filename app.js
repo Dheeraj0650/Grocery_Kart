1597360082912c0778cbb93e5ff982fd2a6d2a2d
V S S DHEERAJ KOTTE <kottedheeraj@gmail.com>
2020-08-07 10:18:15 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -396,8 +396,8 @@ app.get("/payment",function(req,res){
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 9000;
 }
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("connected");
 });
